3c6dc0fec986ab1e30c7d7fa089308a873866b5c
AkshayWay <38669737+AkshayWay@users.noreply.github.com>
2022-09-19 00:57:00 +0530

    Delete payload added

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const controller = require("../controllers/userController");
 const SchemaValidation = require("../SchemaValidation");
 
-router.get("/getUser", controller.getUser);
 router.post("/user", SchemaValidation.apiValidation, controller.postUser);
+router.get("/getUser", controller.getUser);
 router.post("/newUser", controller.postUser);
 router.put("/editUser/:id", controller.editUser);
+router.delete("/deleteUser/:id", controller.deleteUser);
 
 module.exports = router;
